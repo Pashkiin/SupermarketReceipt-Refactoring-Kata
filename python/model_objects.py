3a433c69243db4e708c8b387839e46d115464e30
Pashkiin <rtoit007pele@gmail.com>
2025-12-11 22:26:24 +0100

    remove the smell of feature envy from Shopping_cart by removing discount calculation logic to the Offer class

--- python/model_objects.py
+++ python/model_objects.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import math
 
 
 class Product:
@@ -24,15 +25,57 @@ class SpecialOfferType(Enum):
     TWO_FOR_AMOUNT = 3
     FIVE_FOR_AMOUNT = 4
 
+class Discount:
+    def __init__(self, product, description, discount_amount):
+        self.product = product
+        self.description = description
+        self.discount_amount = discount_amount
+
 class Offer:
     def __init__(self, offer_type, product, argument):
         self.offer_type = offer_type
         self.product = product
         self.argument = argument
 
+    def calculate_discount(self, quantity, unit_price):
+        if self.offer_type == SpecialOfferType.THREE_FOR_TWO:
+            return self._handle_three_for_two(quantity, unit_price)
+        
+        if self.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
+            return self._handle_ten_percent_discount(quantity, unit_price)
+        
+        if self.offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
+            return self._handle_two_for_amount(quantity, unit_price)
+        
+        if self.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
+            return self._handle_five_for_amount(quantity, unit_price)
+            
+        return None
 
-class Discount:
-    def __init__(self, product, description, discount_amount):
-        self.product = product
-        self.description = description
-        self.discount_amount = discount_amount
+    def _handle_three_for_two(self, quantity, unit_price):
+        quantity_as_int = int(quantity)
+        if quantity_as_int <= 2:
+            return None
+        discount_amount = quantity * unit_price - (
+            (math.floor(quantity_as_int / 3) * 2 * unit_price) + quantity_as_int % 3 * unit_price)
+        return Discount(self.product, "3 for 2", -discount_amount)
+
+    def _handle_ten_percent_discount(self, quantity, unit_price):
+        discount_amount = quantity * unit_price * self.argument / 100.0
+        return Discount(self.product, str(self.argument) + "% off", -discount_amount)
+
+    def _handle_two_for_amount(self, quantity, unit_price):
+        quantity_as_int = int(quantity)
+        if quantity_as_int < 2:
+            return None
+        total = self.argument * math.floor(quantity_as_int / 2) + quantity_as_int % 2 * unit_price
+        discount_n = unit_price * quantity - total
+        return Discount(self.product, "2 for " + str(self.argument), -discount_n)
+
+    def _handle_five_for_amount(self, quantity, unit_price):
+        quantity_as_int = int(quantity)
+        if quantity_as_int < 5:
+            return None
+        discount_total = unit_price * quantity - (
+            self.argument * math.floor(quantity_as_int / 5) + quantity_as_int % 5 * unit_price)
+        return Discount(self.product, "5 for " + str(self.argument), -discount_total)
--- python/shopping_cart.py
+++ python/shopping_cart.py
@@ -26,54 +26,12 @@ class ShoppingCart:
             self._product_quantities[product] = quantity
 
     def handle_offers(self, receipt, offers, catalog):
-        for p, quantity in self._product_quantities.items():
-            if p in offers:
-                offer = offers[p]
-                unit_price = catalog.unit_price(p)
-                discount = None
-
-                if offer.offer_type == SpecialOfferType.THREE_FOR_TWO:
-                    discount = self._handle_three_for_two(p, quantity, unit_price)
+        for product, quantity in self._product_quantities.items():
+            if product in offers:
+                offer = offers[product]
+                unit_price = catalog.unit_price(product)
                 
-                elif offer.offer_type == SpecialOfferType.TEN_PERCENT_DISCOUNT:
-                    discount = self._handle_ten_percent_discount(p, quantity, unit_price, offer)
+                discount = offer.calculate_discount(quantity, unit_price)
                 
-                elif offer.offer_type == SpecialOfferType.TWO_FOR_AMOUNT:
-                    discount = self._handle_two_for_amount(p, quantity, unit_price, offer)
-                
-                elif offer.offer_type == SpecialOfferType.FIVE_FOR_AMOUNT:
-                    discount = self._handle_five_for_amount(p, quantity, unit_price, offer)
-
                 if discount:
                     receipt.add_discount(discount)
-
-    def _handle_three_for_two(self, product, quantity, unit_price):
-        quantity_as_int = int(quantity)
-        if quantity_as_int <= 2:
-            return None
-            
-        discount_amount = quantity * unit_price - (
-            (math.floor(quantity_as_int / 3) * 2 * unit_price) + quantity_as_int % 3 * unit_price)
-        return Discount(product, "3 for 2", -discount_amount)
-
-    def _handle_ten_percent_discount(self, product, quantity, unit_price, offer):
-        discount_amount = quantity * unit_price * offer.argument / 100.0
-        return Discount(product, str(offer.argument) + "% off", -discount_amount)
-
-    def _handle_two_for_amount(self, product, quantity, unit_price, offer):
-        quantity_as_int = int(quantity)
-        if quantity_as_int < 2:
-            return None
-
-        total = offer.argument * math.floor(quantity_as_int / 2) + quantity_as_int % 2 * unit_price
-        discount_n = unit_price * quantity - total
-        return Discount(product, "2 for " + str(offer.argument), -discount_n)
-
-    def _handle_five_for_amount(self, product, quantity, unit_price, offer):
-        quantity_as_int = int(quantity)
-        if quantity_as_int < 5:
-            return None
-        
-        discount_total = unit_price * quantity - (
-            offer.argument * math.floor(quantity_as_int / 5) + quantity_as_int % 5 * unit_price)
-        return Discount(product, "5 for " + str(offer.argument), -discount_total)
